927133219d85a52c0a90e5f7985ddf3d4e602429
GerardoZenteno <brallanzenteno@gmail.com>
2024-03-25 20:33:40 -0600

    added routing and lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
 
 const routes: Routes = [
   {
@@ -12,6 +14,16 @@ const routes: Routes = [
     path: "about",
     component: AboutPageComponent
   },
+    {
+    path: "contact",
+    component: ContactPageComponent
+  },
+  // CountriesModule
+  {
+    path: "countries",
+    loadChildren: ()=> import("./countries/countries.module").then(m => m.CountriesModule)
+  },
+
   {
     path: "**",
     redirectTo: ""
