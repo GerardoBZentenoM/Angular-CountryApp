86aae5172d71cbf51c34dc6d30840ea28d9691ca
GerardoZenteno <brallanzenteno@gmail.com>
2024-03-25 19:29:00 -0600

    BigBang

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent
+  },
+  {
+    path: "about",
+    component: AboutPageComponent
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
